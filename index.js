508d1eebcb444e1a5cd7975fa5b68a8fcc1bd795
Nicola Pasquarelli <nicola.pasquarelli46@gmail.com>
2024-05-27 22:42:44 +0200

    modificato index.js aggiunto routes.json

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 const jsonServer = require('json-server');
 const auth = require('json-server-auth');
+const fs = require('fs');
+const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Sostituisci 'db.json' con il tuo file JSON
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-// Imposta i middlewares
+// Leggi le regole dal file routes.json
+const routes = JSON.parse(fs.readFileSync(path.join(__dirname, 'routes.json')));
+
+// Configura il rewriter con le regole
+server.use(jsonServer.rewriter(routes));
+
+// Usa i middlewares e json-server-auth
 server.use(middlewares);
-
-// Regole di json-server-auth
-const rules = auth.rewriter({
-  // Regole di esempio
-  users: 600,
-  messages: 640,
-  posts: 660
-});
-
-// Usa le regole e l'autenticazione
-server.use(rules);
 server.use(auth);
 server.use(router);
 
-// Avvia il server
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
