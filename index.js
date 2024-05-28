8ee406ad560e841e174272cfc360bf9310179ad2
Nicola Pasquarelli <nicola.pasquarelli46@gmail.com>
2024-05-28 18:54:36 +0200

    aggiunto codice per json-server-auth

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+const app = jsonServer.create()
 
 // Leggi le regole dal file routes.json
 const routes = JSON.parse(fs.readFileSync(path.join(__dirname, 'routes.json')));
@@ -21,3 +22,13 @@ server.use(router);
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
+
+
+
+// /!\ Bind the router db to the app
+app.db = router.db
+
+// You must apply the auth middleware before the router
+app.use(auth)
+app.use(router)
+app.listen(3000)
